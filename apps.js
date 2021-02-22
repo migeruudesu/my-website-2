e126a4a393afe3b9fc459ff789695aecac3351bb
miguel.desktop <miguelfm.pro@gmail.com>
2021-02-23 03:13:06 +0800

    first commit

---
+++ apps.js
@@ -0,0 +1,101 @@
+const jsBody = document.querySelector('body');
+const jsHeader = document.querySelector('header');
+const jsHamburger = document.querySelector('.navbar--hamburger');
+const jsHamburgerLine = document.querySelectorAll('.navbar--hamburger--line');
+const jsNavbarLinks = document.querySelector('.navbar--links');
+const jsNavbarLinksa = document.querySelectorAll('.navbar--links-a');
+const jsMediaMin769 = window.matchMedia('(max-width:480px)');
+const jsMlogo = document.querySelector('.mlogo');
+const jsSocialIcon = document.querySelector('.social--container');
+// let jsHamburgerLine02 = document.querySelector('.navbar--hamburger--line');
+
+
+let hamColor = false;
+let logoColor = false;
+function removeShow(currentScreen){
+    if(currentScreen.matches){
+        if(jsNavbarLinks.classList.contains('show')){
+        jsNavbarLinks.classList.remove('show');
+        jsSocialIcon.classList.remove('displayon')
+        // console.log(alsdkj);
+    }
+    // console.log("matched");
+    }
+    
+}
+
+jsMediaMin769.addEventListener('change',removeShow);
+removeShow(jsMediaMin769);
+
+jsHamburger.addEventListener('click',()=>{
+    jsNavbarLinks.classList.toggle('show');
+    // jsMlogo.classList.toggle('changecolor');
+    jsSocialIcon.classList.toggle('displayon')
+    jsBody.classList.toggle('disable');
+    if(!hamColor){
+      setTimeout(changeColor, 100)
+
+    }else{
+      removeColor()
+    }
+    
+})
+
+
+let changeColor =  function(){
+    for(let i = 0; i < jsHamburgerLine.length; i++){
+        jsHamburgerLine[i].style.backgroundColor = "#FDDB27";
+        hamColor = true;
+      };
+      for(let i = 0; i < jsNavbarLinksa.length; i++){
+        jsNavbarLinksa[i].style.color="white";
+        }
+      jsMlogo.style.color="#ff5500";
+      logoColor= true;
+  };
+
+let removeColor =  function(){
+    for(let i = 0; i < jsHamburgerLine.length; i++){
+        jsHamburgerLine[i].style.backgroundColor = null;
+        hamColor = false;
+      }
+    for(let i = 0; i < jsNavbarLinksa.length; i++){
+      jsNavbarLinksa[i].style.color= null;
+      }
+      jsMlogo.style.color=null;
+      logoColor= false;
+  };
+
+  
+window.addEventListener("scroll",()=>{
+    if (document.documentElement.scrollTop > 20) {
+      jsHeader.style.transition = "all .2s";
+      jsHeader.style.backgroundColor = "rgba(0,0,0)";
+      // jsMlogo.style.color = "#ff5500";
+      changeColor();
+    } else if(document.documentElement.scrollTop <= 20) {
+      jsHeader.style.backgroundColor = "transparent";
+      // jsMlogo.style.color = "white";
+      removeColor();
+    }
+})
+
+//  this is make the color of nav--links-a and mlogo retained 
+// when the hamburger is pressed when already scrolled
+
+jsHamburger.addEventListener('click',()=>{
+      // console.log('tae'); test good
+    if(document.documentElement.scrollTop > 1){
+      // console.log('scrolled'); test good
+      if(!logoColor){
+        // console.log('logo color is false'); test good
+        // jsMlogo.style.color = "#ff5500";
+        // logoColor = true;
+        changeColor();
+      }
+    }
+    else {
+      // console.log('not scrolled'); test good
+      // console.log('logo color is true'); test good
+    }
+})
